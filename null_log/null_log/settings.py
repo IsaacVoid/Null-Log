72e78bf2a761827f704cc378a5c6fbc61bcffb1a
IsaacVoid <isaac20.espinosa@gmail.com>
2025-06-19 15:10:34 -0700

    Images
    Configuración para subir imagenes

--- null_log/null_log/settings.py
+++ null_log/null_log/settings.py
@@ -79,7 +79,10 @@ DATABASES = {
     }
 }
 
+# Imagenes 
 
+MEDIA_URL = '/media/'
+MEDIA_PORT = BASE_DIR / 'media'
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
