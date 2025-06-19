547eabfcbeb47ed6749b0f61340a06c568acb0c5
IsaacVoid <isaac20.espinosa@gmail.com>
2025-06-19 15:15:38 -0700

    Vistas y Template
    Creacion de vistas y Templates

--- null_log/posts/views.py
+++ null_log/posts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 # Create your views here.
+def inicio(request):
+    post = Post.object.all().orde_by('fecha_creacion')
+    return render(request, 'inicio.html', {'posts': posts})
+
+def detale_post(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    return render(request, 'detalle_post.html', {'post': post})
